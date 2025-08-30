9e881a2362cd02aabadb64fb18a0daea3a7f101b
bhoi hitesh <bhoihitesh1492@gmail.com>
2025-08-30 11:13:25 +0530

    typescript build error fixed

--- src/app/ui-elements/alerts/page.tsx
+++ src/app/ui-elements/alerts/page.tsx
@@ -14,21 +14,24 @@ export default function Page() {
 
       <div className="space-y-7.5 rounded-[10px] bg-white p-4 shadow-1 dark:bg-gray-dark dark:shadow-card md:p-6 xl:p-9">
         <Alert
-          variant="warning"
-          title="Attention Needed"
-          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when"
+        show={true}
+        variant="warning"
+        title="Attention Needed"
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when"
         />
 
         <Alert
-          variant="success"
-          title="Message Sent Successfully"
-          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        show={true}
+        variant="success"
+        title="Message Sent Successfully"
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
         />
 
         <Alert
-          variant="error"
-          title="There were 1 errors with your submission"
-          description="Lorem Ipsum is simply dummy text of the printing"
+        show={true}
+        variant="error"
+        title="There were 1 errors with your submission"
+        description="Lorem Ipsum is simply dummy text of the printing"
         />
       </div>
     </>
